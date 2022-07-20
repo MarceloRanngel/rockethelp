b16d169ee6eb012a6364cf72830e920137957bec
Marcelo Rangel <Marcelo.rcs@icloud.com>
2022-07-20 20:18:01 -0300

    Add Loading Screen for Font Load

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { SignIn } from './src/screens/SignIn';
 import { NativeBaseProvider } from 'native-base';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
 import { THEME } from './styles/theme';
 
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn/>
+      {fontsLoaded ? <SignIn/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
