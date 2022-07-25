5bbcadadf09614df24007f7cd1150454fb70fab4
Marcelo Rangel <Marcelo.rcs@icloud.com>
2022-07-25 16:18:17 -0300

    Add navigation on Buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { HStack, IconButton, useTheme, StyledProps, Heading } from 'native-base';
 import { CaretLeft } from 'phosphor-react-native';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = StyledProps & {
     title: string;
@@ -7,6 +8,11 @@ type Props = StyledProps & {
 
 export function Header({title, ...rest}: Props) {
     const {colors} = useTheme();
+    const navigation  = useNavigation();
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
 
     return (
     <HStack 
@@ -18,7 +24,7 @@ export function Header({title, ...rest}: Props) {
         pt={12}
         {...rest}
     >
-        <IconButton icon={ <CaretLeft color={colors.gray[200]} size={24}/>}/>
+        <IconButton icon={ <CaretLeft color={colors.gray[200]} size={24}/>} onPress={handleGoBack}/>
         <Heading color={colors.gray[100]} textAlign="center" fontSize="lg" flex={1} ml={-6}>
             {title}
         </Heading>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,7 @@ import Logo from '../assets/logo_secondary.svg';
 import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order';
+import { useNavigation } from '@react-navigation/native';
 
 export function Home() {
 
@@ -14,11 +15,21 @@ export function Home() {
     const [orders, setOrders] = useState<OrderProps[]>([{
         id: '123',
         patrimony: '123456',
-        when: '21/07/2022',
+        when: '21/07/2022 às 14:00',
         status: 'open'
-    }])
+    },])
 
     const {colors} = useTheme();
+    const navigation = useNavigation();
+
+
+    function handleNewOrder() {
+        navigation.navigate('new')
+    }
+
+    function handleOpenDetails(orderId: string){
+        navigation.navigate('details', { orderId })
+    }
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -41,7 +52,7 @@ export function Home() {
                     Solicitações
                 </Heading>
                 <Text color="gray.200">
-                    3
+                    {orders.length}
                 </Text>
             </HStack>
             <HStack space={3} mb={8}>
@@ -61,7 +72,7 @@ export function Home() {
             <FlatList
                 data={orders}
                 keyExtractor={item => item.id}
-                renderItem={({item}) => <Order data={item}/>}
+                renderItem={({item}) => <Order data={item} onPress={() => handleOpenDetails(item.id)}/>}
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{paddingBottom: 100}}
                 ListEmptyComponent={() => (
@@ -74,7 +85,7 @@ export function Home() {
                     </Center>
                 )}
             />
-            <Button title="Nova Solicitação"/>
+            <Button title="Nova Solicitação" onPress={handleNewOrder}/>
         </VStack>
     </VStack>
   );
